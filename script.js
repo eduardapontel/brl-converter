051a6c9ca835fec449ad3b029ed4b1687ff8273e
Eduarda Alves Pontel <109531518+eduardapontel@users.noreply.github.com>
2026-09-09 17:15:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const USD = 4.87;
-const EUR = 5.32;
-const GBP = 6.08;
+const USD = 5.09;
+const EUR = 5.93;
+const GBP = 6.90;
 
 const form = document.querySelector('form');
 const amount = document.querySelector('#amount');
